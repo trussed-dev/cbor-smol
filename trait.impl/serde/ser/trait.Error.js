f16b50a25fc24d9ae6f92bdc08a90ecc8560fd06
robin-nitrokey <robin-nitrokey@users.noreply.github.com>
2024-10-24 18:38:36 +0000

    deploy: bd92517ceca6cebe4cb7d6ab68345bb7b542a43b

--- trait.impl/serde/ser/trait.Error.js
+++ trait.impl/serde/ser/trait.Error.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"cbor_smol":[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.210/serde/ser/trait.Error.html\" title=\"trait serde::ser::Error\">Error</a> for <a class=\"enum\" href=\"cbor_smol/error/enum.Error.html\" title=\"enum cbor_smol::error::Error\">Error</a>"]]
+"cbor_smol":[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.213/serde/ser/trait.Error.html\" title=\"trait serde::ser::Error\">Error</a> for <a class=\"enum\" href=\"cbor_smol/error/enum.Error.html\" title=\"enum cbor_smol::error::Error\">Error</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
